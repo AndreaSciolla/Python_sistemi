081da24e11c9a401adea23b6172ab32dea222c56
AndreaSciolla <andrea.sciolla@itiscuneo.eu>
2023-10-03 15:23:03 +0200

    Update ES_4.py
    spiegazione dello scambio di variabili

--- ES_4.py
+++ ES_4.py
@@ -2,7 +2,8 @@ def main():
     x = float(input("inserisci il primo numero:"))
     y = float(input("inserisci il secondo numero:"))
     if x>y:
-        y, x = x, y
+        y, x = x, y #algoritmo di scambio (non servono variabili temporanee)
+        #assegnamento multiplo:   int a, b = 5, 7 (int a = 5; int b = 7)
     print(f"{y}, {x}")
 if __name__ == "__main__":
     main()
