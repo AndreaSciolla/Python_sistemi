c74a2a54c2c8cf335cdde9215b5eaaf008e908cd
AndreaSciolla <andrea.sciolla@itiscuneo.eu>
2023-10-09 08:09:12 +0200

    Create ES_9_3_TURTLE.py
    creazione del es 2 con Python e il modulo turtle

---
+++ ES_9_3_TURTLE.py
@@ -0,0 +1,33 @@
+#DISEGNARE UNA STELLA A N PUNTE
+import turtle
+import math
+
+n = int(input("inserisci il numero di punte: "))
+finestra = turtle.Screen()
+alice = turtle.Turtle()
+lato = 50
+
+if n != 0 and n !=1:
+    if(n%2)!=0:
+        for i in range(0, n):
+            alice.forward(lato)
+            alice.left(720/n)
+    else:
+        for k in range(0, int(n/2)):
+            alice.forward(lato)
+            alice.left(720/n)
+        alice.penup()
+        alice.forward(lato/2)
+        alice.right(90)
+        alice.forward(((lato/3)/math.sqrt(2))/(n/8))  #formula per  trovale cateto del mezzo triangolo che forma punta della stella
+        alice.left(360/n + 90)
+        alice.pendown()
+        for j in range(0, int(n/2)):
+            alice.forward(lato)
+            alice.left(720/n)
+   
+
+else:
+    print("il numero di punte inserito non è valido")
+
+finestra.mainloop()
