40a6e77824a950b6396bbe0970e31c85682772e0
AndreaSciolla <andrea.sciolla@itiscuneo.eu>
2023-10-23 08:09:36 +0200

    Create PAG_55_ES_18.py

---
+++ PAG_55_ES_18.py
@@ -0,0 +1,4 @@
+num1 = int(input("inserisci un numero: "))
+num2 = int(input("inserisci un numero: "))
+lista = [ (num1**2) + (num2**2), (num1+num2)**2, (num1**2) - (num2**2), (num1-num2)**2]
+print(lista)
