90f1a9e12644da3d3df3cde0fef727ca73c5b3f3
AndreaSciolla <andrea.sciolla@itiscuneo.eu>
2023-10-23 11:27:29 +0200

    Update PAG_73_ES_21.py
    correzione in classe

--- PAG_73_ES_21.py
+++ PAG_73_ES_21.py
@@ -1,3 +1,3 @@
-stringa = "ciao questa e una prova"
+stringa = "ciaoquestaeunaprova"
 stringa_no_voc = [let for let in stringa if (let != 'a' and let !='e' and let !='i' and let !='o' and let !='u')]
-print(stringa_no_voc)
+print("".join(stringa_no_voc)) #join attacca una lista di stringhe usando come carattere separatore quello tra ""
