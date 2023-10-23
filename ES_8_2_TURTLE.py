fdf07fef29c42611504245b6a1200e070db5dddb
AndreaSciolla <andrea.sciolla@itiscuneo.eu>
2023-10-23 08:09:47 +0200

    Update ES_8_2_TURTLE.py

--- ES_8_2_TURTLE.py
+++ ES_8_2_TURTLE.py
@@ -1,30 +1,39 @@
 import turtle
 #stampare i primi 9 poligoni sulla finestra
 
-finestra = turtle.Screen()
-alice = turtle.Turtle()
-alice.color('red', 'blue')
-alice.begin_fill()
-per = 400
-alice.penup()
-alice.backward(150)
-alice.left(90)
-alice.forward(150)
-alice.right(90)
-alice.pendown()
-for i in range(3, 12):
-    alice.pendown()
+def disegnaPoligono(t, num, lato):
     for k in range(0, i):
         alice.left(360/i)
-        alice.forward(per/i)
-    alice.penup()
-    alice.forward(150)
-    if i != 3:
-        if ((i-2)%3)== 0:
-            alice.penup()
-            alice.right(90)
-            alice.forward(155)
-            alice.left(90)
-            alice.backward(470)
+        alice.forward(lato/i)
 
-finestra.mainloop()
+#def posizionaTurtle():
+    
+
+def main():
+
+    finestra = turtle.Screen()
+    alice = turtle.Turtle()
+    alice.shape("turtle")
+    alice.color('red', 'blue')#cambia il colore della linea
+    per = 400
+    alice.penup()
+    alice.backward(150)
+    alice.left(90)
+    alice.forward(150)
+    alice.right(90)
+    alice.pendown()
+    for i in range(3, 12):
+        alice.pendown()
+        disegnaPoligono(alice, i, per)
+        alice.penup()
+        alice.forward(150)
+        if i != 3:
+            if ((i-2)%3)== 0:
+                alice.penup()
+                alice.right(90)
+                alice.forward(155)
+                alice.left(90)
+                alice.backward(470)
+    finestra.mainloop()
+if __name__ == "__main__":
+    main()
