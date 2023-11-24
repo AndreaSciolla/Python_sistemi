d449b395595dff93119927bcb330631bfa39987c
AndreaSciolla <andrea.sciolla@itiscuneo.eu>
2023-11-24 08:42:50 +0100

    Update PAG_73_ES_18.py

--- PAG_73_ES_18.py
+++ PAG_73_ES_18.py
@@ -1,6 +1,7 @@
 import math
 def main():
     lista = [i**2 for i in range(0, int(math.sqrt(200))) if(i*i % 2 != 0)]
+    #lista = [i**2 for i in range(0, int(math.sqrt(200))) if(i*i % 2 != 0)]
     print(lista)
-    if __name__ == "__main__":
+if __name__ == "__main__":
         main()
