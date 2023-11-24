9433372ebafc8720c9a93791abf951d6638a68fd
AndreaSciolla <andrea.sciolla@itiscuneo.eu>
2023-11-24 08:44:15 +0100

    Create ES_IP_SUB_BRO.py

---
+++ ES_IP_SUB_BRO.py
@@ -0,0 +1,32 @@
+#calcola ip di rete e ip di boradcast.
+
+
+def completa8bit(strbin):
+    b = strbin[2:]
+    l = len(b)
+    return ("0"*(8-l) + b)
+
+
+ipAddress = input("inserisci indirizzo ip: ")
+#ipAddress_bin = 
+sub_num = int(input("inserisci subnet mask in CIDR: "))
+sub_bin = "1"*sub_num + "0"*(32 - sub_num)
+wild_bin = "0"*sub_num + "1"*(32 - sub_num)
+
+
+
+groups = ipAddress.split(".") #lista che contiene i gruppi; la split ritorna una lista di stringhe
+groups = [int(group) for group in groups] #convertire i gruppi da stringhe a interi
+groups_b = [bin(elemento) for elemento in groups]
+groups_strbin = [completa8bit(strbin) for strbin in groups_b]
+ipAddress_bin = "".join(groups_strbin)
+rete_str = ["1" if (a == "1" and b == "1") else "0" for a, b in zip(ipAddress_bin, sub_bin)]
+broad_str = ["1" if (a == "1" or b == "1") else "0" for a, b in zip(ipAddress_bin, wild_bin)]
+rete_bin = "".join(rete_str)
+broad_bin = "".join(broad_str)
+list_rete_bin = [rete_bin[i:i+8] for i in range(0, 32, 8)]
+list_broad_bin = [broad_bin[i:i+8] for i in range(0, 32, 8)]
+list_rete_dec = [int(group, 2) for group in list_rete_bin]
+list_broad_dec = [int(group, 2) for group in list_broad_bin]
+print(f"rete: {list_rete_dec}")
+print(f"broadcast: {list_broad_dec}")
