69fffcc9b5a072cfb07eb51b2dc8e87f2d2f28d6
AndreaSciolla <andrea.sciolla@itiscuneo.eu>
2024-02-18 16:52:17 +0100

    Create SPIEGAZIONE_TUPLE.py

---
+++ SPIEGAZIONE_TUPLE.py
@@ -0,0 +1,10 @@
+#SPIEGAZIONE TUPLE
+
+t = (1, 4, 10, 3) #una tupla è una collezione di elementi
+print(t[2]) #output: 10
+# t[1] = 9  ->non si può fare perche le tuple sono immutabili
+
+#esempio
+punto = (3, 4)
+x, y = punto #assegnazione multipla -> questo tipo si chiama spacchettamento della tupla (il numero di elementi deve essere uguale a variabili che si vogliono assegnare)
+print(x)
